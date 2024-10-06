fe1c0ef3c0b72f0944e78c77bc1bc5532b53d4d0
Kieran Merrett <116359190+KieMerr17@users.noreply.github.com>
2024-10-06 15:53:50 -0700

    update categories to include sub categories

--- categories.py
+++ categories.py
@@ -1,19 +1,139 @@
-
-categories = [
-    "Failed to Accelerate",
-    "Failed to Slow",
-    "Failed to remain Stopped",
-    "Failed to maintain Speed",
-    "Failed to follow Route",
-    "Failed to Overtake",
-    "Failed for Early Turn",
-    "Failed for Late Turn",
-    "Failed for Lane Position",
-    "Failed for Diversion",
-    "Emergency Services",
-    "Close Proximity",
-    "Uncommanded Disengagement",
-    "Emergency Stop",
-    "Accidental Disengagement",
-    "End of run"
-]
+categories = {
+    "Failed to Accelerate": [
+        "Empty Zebra Crossing",
+        "Yellow Box",
+        "Green Light",
+        "Turning",
+        "Giveway",
+        "Match Traffic Speed",
+        "Move Off in Traffic",
+        "Keep Clear Markings",
+        "To Speed Limit",
+        "Stop Junction",
+        "Following Narrow Road"
+    ],
+    "Failed to Slow": [
+        "Zebra Crossing",
+        "Jaywalker",
+        "Match Traffic Speed",
+        "Stopped Across Crossing",
+        "Bus at Bus Stop",
+        "Narrow Road",
+        "Red Light",
+        "Amber Light",
+        "Above Speed Limit",
+        "Junction",
+        "Roadworks",
+        "Refuge Truck",
+        "Speed Bump",
+        "Merging Vehicle",
+        "Cyclist",
+        "Keep Clear Markings",
+        "Pedestrian",
+        "Width Restriction",
+        "Yellow Box",
+        "Behind Stationary Vehicle",
+        "Stop Junction"
+    ],
+    "Failed to Remain Stopped": [
+        "Red Light",
+        "Zebra Crossing",
+        "Stationary Traffic",
+        "Narrow Road",
+        "Waiting to Turn",
+        "Junction",
+        "Yellow Box",
+        "Keep Clear Markings",
+        "Amber Light"
+    ],
+    "Failed to Maintain Speed": [
+        "Maintain the Speed Limit",
+        "Match the Traffic Speed",
+        "Empty Zebra Crossing",
+        "Turning",
+        "Green Light",
+        "Slowed Too Early"
+    ],
+    "Failed to Follow Route": [
+        "Failed to Turn",
+        "Took Unplotted Turn",
+        "Incorrect Lane for Upcoming Turn",
+        "Incorrect Lane to Continue Ahead",
+        "Incorrect Indicator"
+    ],
+    "Failed to Overtake": [
+            "Bus",
+            "Single Vehicle",
+            "Line of Parked Vehicles",
+            "Cyclist",
+            "Motorcyclist",
+            "Bin Lorry",
+            "Ongoing Vehicle",
+            "Double Parked Vehicle",
+            "Other Static",
+            "Other Dynamic"
+    ],
+    "Failed for Early Turn": [
+        "Towards Oncoming Lane",
+        "Towards Ongoing Lane",
+        "Towards Cycle Lane",
+        "Towards Bus Lane",
+        "Towards Cyclist",
+        "Towards Motorcyclist",
+        "Towards Kerb",
+        "Towards Pedestrian",
+        "Towards Vehicle",
+        "Towards Static Obstacle",
+        "Towards Other Static",
+        "Towards Other Dynamic"
+    ],
+    "Failed for Late Turn": [
+        "Towards Oncoming Lane",
+        "Towards Ongoing Lane",
+        "Towards Cycle Lane",
+        "Towards Bus Lane",
+        "Towards Cyclist",
+        "Towards Motorcyclist",
+        "Towards Kerb",
+        "Towards Pedestrian",
+        "Towards Vehicle",
+        "Towards Static Obstacle",
+        "Towards Other Static",
+        "Towards Other Dynamic"
+    ],
+    "Failed for Lane Position": [
+        "Towards Oncoming Lane",
+        "Towards Ongoing Lane",
+        "Towards Cycle Lane",
+        "Towards Bus Lane",
+        "Towards Cyclist",
+        "Towards Motorcyclist",
+        "Towards Kerb",
+        "Towards Pedestrian",
+        "Towards Vehicle",
+        "Towards Static Obstacle",
+        "Towards Other Static",
+        "Towards Other Dynamic"
+    ],
+    "Failed for Diversion": [
+        "Failed for Diversion"
+    ],
+    "Emergency Services": [
+        "Emergency Services"
+    ],
+    "Close Proximity": [
+        "Close Proximity"
+    ],
+    "Uncommanded Disengagement": [
+        "Uncommanded Disengagement"
+    ],
+    "Emergency Stop": [
+        "Emergency Stop"
+    ],
+    "Accidental Disengagement": [
+        "Accidental Disengagement"
+    ],
+    "End of Run": [
+        "End of Run"
+    ]
+}
