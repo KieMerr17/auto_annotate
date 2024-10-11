c0a992fd10e9aac0865b872afae80d96d5f30f43
Kieran Merrett <116359190+KieMerr17@users.noreply.github.com>
2024-10-10 21:43:50 -0700

    remove categories.py and add to auto_annotate.py

--- auto_annotate.py
+++ auto_annotate.py
@@ -5,7 +5,219 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import pickle
 import logging
-from categories import categories
+from env import openai_API_key
+
+
+categories = {
+    "Failed to Accelerate": [
+        "Empty Zebra Crossing",
+        "Yellow Box",
+        "Green Light",
+        "Turning",
+        "Give Way",
+        "Move Off in Traffic",
+        "After Negotiating Traffic on Narrow Road",
+        "Keep Clear Markings",
+        "Stop Junction",
+        "Roundabout",
+        "Pedestrian/Jaywalker Finished",
+        "Clear Road",
+        "Four Way Stop",
+        "Turning Right on Red"
+    ],
+    "Failed to Slow": [
+        "Above Speed Limit",
+        "Approaching Junction",
+        "Amber Light",
+        "Approaching Potholes",
+        "Red Light",
+        "At a Giveway",
+        "Oncoming Vehicles",
+        "Behind a Refuse Truck Working at the Roadside",
+        "Non Priority Vehicles",
+        "Behind Vehicle Stationary in Traffic",
+        "Cyclist",
+        "Other Dynamic",
+        "Jaywalking",
+        "Stop Sign",
+        "Primary Stop Line",
+        "Undertaking",
+        "Exceed Driver Set Speed",
+        "Keeping Clear Markings",
+        "Pedestrian on Zebra Crossing",
+        "Match Speed of Traffic",
+        "Pedestrian at Junction or Informal Crossing Point",
+        "Negotiating Traffic on Narrow Road",
+        "Behind a Bus at a Bus Stop",
+        "Roadworks",
+        "Stop Junction",
+        "Speed Bump",
+        "Stopped Across Pedestrian Crossing",
+        "Vehicle Merging into Lane",
+        "Width Restriction",
+        "Yellow Box"
+    ],
+    "Failed to Remain Stopped": [
+        "Amber Light",
+        "Junction",
+        "Keep Clear Markings",
+        "Negotiating Traffic on Narrow Roads",
+        "Pedestrian on Zebra Crossing",
+        "Red Light",
+        "Stationary Traffic",
+        "Waiting to Turn Across Traffic",
+        "Yellow Box",
+        "Pedestrian at Junction or Informal Crossing Point",
+        "Jaywalking",
+        "Oncoming Vehicles",
+        "At a Giveway",
+        "Stop Junction",
+        "Non Priority Vehicles",
+        "Cyclist",
+        "Primary Stop Line",
+        "Rollback",
+        "Four Way Stop",
+        "Red Arrow"
+    ],
+    "Failed to Maintain Speed": [
+        "Empty Zebra Crossing",
+        "Green Light",
+        "Match Traffic Speed",
+        "Slowed Too Early",
+        "Stay at Speed Limit",
+        "Turning",
+        "Exceeding Speed Limit",
+        "Variable Speed Limit"
+    ],
+    "Failed to Follow Route": [
+        "Failed to Turn into Plotted Turn",
+        "Incorrect Indicator",
+        "Incorrect Lane for Continue Ahead",
+        "Incorrect Lane for Upcoming Turn",
+        "Took Unplotted Turn"
+    ],
+    "Failed to Overtake": [
+        "Incorrectly Initiated Lorry",
+        "Failed to Initiate Lorry",
+        "Failed to Complete Lorry",
+        "Incorrectly Initiated Bus",
+        "Failed to Initiate Bus",
+        "Failed to Complete Bus",
+        "Incorrectly Initiated Cyclist",
+        "Failed to Initiate Cyclist",
+        "Failed to Complete Cyclist",
+        "Incorrectly Initiated Double Parked Vehicle",
+        "Failed to Initiate Double Parked Vehicle",
+        "Failed to Complete Double Parked Vehicle",
+        "Incorrectly Initiated Line of Parked Vehicles",
+        "Failed to Initiate Line of Parked Vehicles",
+        "Failed to Complete Line of Parked Vehicles",
+        "Incorrectly Initiated Motorcycle",
+        "Failed to Initiate Motorcycle",
+        "Failed to Complete Motorcycle",
+        "Incorrectly Initiated Ongoing Vehicle Waiting to Turn",
+        "Failed to Initiate Ongoing Vehicle Waiting to Turn",
+        "Failed to Complete Ongoing Vehicle Waiting to Turn",
+        "Incorrectly Initiated Other (Dynamic)",
+        "Failed to Initiate Other (Dynamic)",
+        "Failed to Complete Other (Dynamic)",
+        "Incorrectly Initiated Other (Static)",
+        "Failed to Initiate Other (Static)",
+        "Failed to Complete Other (Static)",
+        "Incorrectly Initiated Single Parked Vehicle",
+        "Failed to Initiate Single Parked Vehicle",
+        "Failed to Complete Single Parked Vehicle"
+    ],
+    "Failed to Follow Lane Position": [
+        "Lane Position",
+        "Junction - Incorrect Position",
+        "Lane Position",
+        "Erratic Steering",
+        "Lane Position",
+        "Too Close to Kerb",
+        "Towards Cyclist",
+        "Towards Motorcyclist",
+        "Towards Kerb",
+        "Towards Oncoming Lane",
+        "Towards Ongoing Lane",
+        "Towards Ongoing Restricted Bus Lane",
+        "Towards Ongoing Restricted Cycle Lane",
+        "Towards Other (Dynamic)",
+        "Towards Other (Static)",
+        "Towards Parked Vehicle",
+        "Towards Jaywalking",
+        "Towards Oncoming Vehicle",
+        "Towards Hard Shoulder",
+        "Failed to Complete a Turn",
+        "Weaving in a Lane"
+    ],
+    "Failed for Late Turn": [
+        "Towards Cyclist",
+        "Towards Motorcyclist",
+        "Towards Kerb",
+        "Towards Oncoming Lane",
+        "Towards Ongoing Lane",
+        "Towards Ongoing Restricted Bus Lane",
+        "Towards Ongoing Restricted Cycle Lane",
+        "Towards Other (Dynamic)",
+        "Towards Other (Static)",
+        "Towards Parked Vehicle",
+        "Towards Pedestrian",
+        "Towards Roundabout"
+    ],
+    "Failed for Early Turn": [
+        "Towards Cyclist",
+        "Towards Motorcyclist",
+        "Towards Kerb",
+        "Towards Oncoming Lane",
+        "Towards Ongoing Lane",
+        "Towards Ongoing Restricted Bus Lane",
+        "Towards Ongoing Restricted Cycle Lane",
+        "Towards Other (Dynamic)",
+        "Towards Other (Static)",
+        "Towards Parked Vehicle",
+        "Towards Pedestrian",
+        "Towards Roundabout"
+    ],
+    "Uncommanded Disengagement": [
+        "Uncommanded Disengagement"
+    ],
+    "End of Run": [
+        "End of Run"
+    ],
+    "Failed to Initiate Manoeuvre": [
+        "Failed to Initiate Manoeuvre"
+    ],
+    "Close Proximity": [
+        "Close Proximity"
+    ],
+    "Lens Obscured": [
+        "Lens Obscured"
+    ],
+    "Unprompted Manoeuvre": [
+        "Unprompted Manoeuvre"
+    ],
+    "Lane Change": [
+        "Lane Change"
+    ],
+    "Emergency Stop": [
+        "Emergency Stop"
+    ],
+    "Accidental AVSO Intervention": [
+        "Accidental AVSO Intervention"
+    ],
+    "Uncategorised": [
+        "Uncategorised"
+    ],
+    "Diversion": [
+        "Diversion"
+    ],
+    "Emergency Service": [
+        "Emergency Service"
+    ]
+}
+
+
 
 # Function to load existing data from CSV and handle potential parsing errors
 def load_existing_data(filename="data.csv"):
